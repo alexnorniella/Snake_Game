3d6aec07803f49390c20fd217c02c954a248caac
alexnorniella <alexnorniella@yahoo.co.uk>
2024-09-11 14:06:07 +0100

    First commit, window game added, grid, snake and snake's food

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+
+//import this library to Make Game
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 600;
+        int boardHeight = boardWidth;
+        // Set the window Frame
+        JFrame frame = new JFrame ("Snake");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        SnakeGame snakeGame = new SnakeGame(boardWidth,boardHeight);
+        frame.add(snakeGame);
+        frame.pack();
+
+    }
+
+}
---
+++ src/main/java/SnakeGame.java
@@ -0,0 +1,64 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class SnakeGame extends JPanel {
+    private class Tile{
+        int x;
+        int y;
+        Tile(int x, int y){
+            this.x = x;
+            this.y = y;
+
+        }
+    }
+    int boardWidth;
+    int boardHeight;
+    int tileSize = 25;
+
+    //Snake
+    Tile snakeHead;
+
+    //Food
+    Tile food;
+
+
+    SnakeGame(int boardWidth, int boardHeight){
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.black);
+
+        snakeHead = new Tile(5,5);
+
+        food = new Tile(10,10);
+
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g){
+        // Grid
+        g.setColor(Color.gray);
+        for(int i = 0; i< boardWidth/tileSize; i++){
+            //(x1, y1, x2, y2)
+            g.drawLine(i*tileSize, 0, i*tileSize, boardHeight);
+            g.drawLine(0, i*tileSize, boardWidth, i*tileSize);
+
+        }
+        // Food
+
+        g.setColor(Color.red);
+        g.fillRect(food.x * tileSize, food.y * tileSize , tileSize,tileSize);
+        //Snake
+        g.setColor(Color.green);
+        g.fillRect(snakeHead.x *tileSize, snakeHead.y *tileSize,tileSize,tileSize);
+
+    }
+
+}
